198c38f0a9a8c6dc7d30e857857b4519b478d72f
Andreas Copan <avcopan@gmail.com>
2023-07-18 17:21:20 -0400

    Sets up basic template

--- app.py
+++ app.py
@@ -3,30 +3,22 @@
 Run using `flask run`
 """
 from flask import Flask
-import operator
+from flask import render_template
+from toy import Toy
 
 app = Flask(__name__)
+
+duplo = Toy(name='duplo')
+lego = Toy(name='lego')
+knex = Toy(name='knex')
+
+toys = [duplo, lego, knex]
 
 # listen for a route to '/' (root route)
 @app.route('/')
 def hello():
   return "Hello!"
 
-@app.route('/name/<person>')
-def say_name(person):
-  return f"The name is {person}"
-
-@app.route('/name/<int:num>')
-def favorite_number(num):
-  return f"Your favorite number is {num}, which is half of {num * 2}"
-
-@app.route('/math/<operation>/<int:num1>/<int:num2>')
-def math(operation, num1, num2):
-  op_dct = {
-    'add': operator.add,
-    'sub': operator.sub,
-    'mul': operator.mul,
-    'div': operator.floordiv,
-  }
-  op_key = operation.lower()[:3]
-  return str(op_dct[op_key](num1, num2))
+@app.route('/toys')
+def index():
+  return render_template("index.html", toys=toys)
