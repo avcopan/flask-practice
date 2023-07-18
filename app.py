02c1bc482e69a0c752dd89bd30cdb85c73aa72c4
Andreas Copan <avcopan@gmail.com>
2023-07-18 16:45:55 -0400

    Routes with parameters

--- app.py
+++ app.py
@@ -3,26 +3,30 @@
 Run using `flask run`
 """
 from flask import Flask
+import operator
 
 app = Flask(__name__)
 
 # listen for a route to '/' (root route)
 @app.route('/')
 def hello():
-  return "Hello you!"
+  return "Hello!"
 
-@app.route('/name')
-def welcome():
-  return "Welcome to our application!"
+@app.route('/name/<person>')
+def say_name(person):
+  return f"The name is {person}"
 
-@app.route('/welcome')
-def welcome1():
-  return "Welcome"
+@app.route('/name/<int:num>')
+def favorite_number(num):
+  return f"Your favorite number is {num}, which is half of {num * 2}"
 
-@app.route('/welcome/home')
-def welcome2():
-  return "Welcome home."
-
-@app.route('/welcome/back')
-def welcome3():
-  return "Welcome back!"
+@app.route('/math/<operation>/<int:num1>/<int:num2>')
+def math(operation, num1, num2):
+  op_dct = {
+    'add': operator.add,
+    'sub': operator.sub,
+    'mul': operator.mul,
+    'div': operator.floordiv,
+  }
+  op_key = operation.lower()[:3]
+  return str(op_dct[op_key](num1, num2))
